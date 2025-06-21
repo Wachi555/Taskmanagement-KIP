490d0f2802a03fcb25c95ec73a14de88d4161712
Wachi555 <110466235+Wachi555@users.noreply.github.com>
2025-06-21 16:19:07 +0200

    krise

--- backend/src/modules/helpers.py
+++ backend/src/modules/helpers.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def calculate_age(birth_year: str) -> int:
     try:
-        birth_date = datetime.strptime(birth_year, "%Y.%d.%m")
+        birth_date = datetime.strptime(birth_year, "%Y-%d-%m")
         today = datetime.now()
         age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
         return age
